d0e12575c83eebae2f8323ab68b8576356c5d644
Steven R. Loomis <srl295@gmail.com>
2024-10-18 09:52:37 -0500

    polish it

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('keyman.compileProject', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Keyman Project File Editor!');
+		vscode.window.showInformationMessage('Compiled Project from Keyman Project File Editor!');
 	});
 
 	context.subscriptions.push(disposable);
