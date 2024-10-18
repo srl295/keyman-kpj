f5198e7aca40b8bd0085e8abd47012f30f2b5bcf
Steven R. Loomis <srl295@gmail.com>
2024-10-18 12:08:27 -0500

    feat: builds!

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,89 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path from 'node:path';
+import { existsSync } from 'node:fs';
 import * as vscode from 'vscode';
+
+let kpjPromise: Thenable<vscode.Task[]> | undefined = undefined;
+
+interface KpjTaskDefinition extends vscode.TaskDefinition {
+	// empty for now
+}
+
+async function getKpjTasks(): Promise<vscode.Task[]> {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	const result: vscode.Task[] = [];
+	if (!workspaceFolders || workspaceFolders.length === 0) {
+		return result;
+	}
+	for (const workspaceFolder of workspaceFolders) {
+		const folderString = workspaceFolder.uri.fsPath;
+		if (!folderString) {
+			console.log(`Not folstr`);
+			continue;
+		}
+		const dir = path.basename(folderString);
+		const kpjFile = path.join(folderString, `${dir}.kpj`);
+		if (!existsSync(kpjFile)) {
+			console.log(`Not exist ${kpjFile}`);
+			continue;
+		} else {
+			console.log(`=> ${kpjFile}`);
+		}
+		const task = new vscode.Task({type: 'kpj'}, workspaceFolder, dir, 'kpj',
+				new vscode.ShellExecution(`npx -y @keymanapp/kmc build file ${kpjFile}`));
+		task.group = vscode.TaskGroup.Build;
+		console.dir({task});
+		result.push(task);
+	}
+	return result;
+}
+let kpjTaskProvider : vscode.Disposable | undefined;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let NEXT_TERM_ID = 1;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	// console.log("Terminals: " + (<any>vscode.window).terminals.length);
+
+	// vscode.window.onDidOpenTerminal(terminal => {
+	// 	console.log("Terminal opened. Total count: " + (<any>vscode.window).terminals.length);
+	// });
+	// vscode.window.onDidOpenTerminal((terminal: vscode.Terminal) => {
+	// 	vscode.window.showInformationMessage(`onDidOpenTerminal, name: ${terminal.name}`);
+	// });
+
+	// // vscode.window.onDidChangeActiveTerminal
+	// vscode.window.onDidChangeActiveTerminal(e => {
+	// 	console.log(`Active terminal changed, name=${e ? e.name : 'undefined'}`);
+	// });
+
+	kpjTaskProvider = vscode.tasks.registerTaskProvider('kpj', {
+		provideTasks() {
+			return (kpjPromise = kpjPromise ?? getKpjTasks());
+		},
+		resolveTask(_task: vscode.Task) : vscode.Task | undefined {
+			const task = _task.definition.task;
+			if (task) {
+				const definition: KpjTaskDefinition = <any>_task.definition;
+				return new vscode.Task(
+					definition,
+					_task.scope ?? vscode.TaskScope.Workspace,
+					definition.type, // for now, se don't have another name to use
+					'kpj',
+					new vscode.ShellExecution(`npx -y @keymanapp/kmc build`) // nothing from the definition for now
+				);
+			}
+		}
+	});
+
 	console.log('Congratulations, your extension "keyman-kpj" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('keyman.compileProject', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+		// const terminal = vscode.window.createTerminal(`Keyman Compile #${NEXT_TERM_ID++}`);
+		// terminal.show();
+		// terminal.sendText("echo 'Sent text immediately after creating'", true);
 		vscode.window.showInformationMessage('Compiled Project from Keyman Project File Editor!');
 	});
 
@@ -23,4 +91,17 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (kpjTaskProvider) {
+		kpjTaskProvider.dispose();
+	}
+	console.log(`Keyman: goodbye!`);
+}
+
+// function ensureTerminalExists(): boolean {
+// 	if ((<any>vscode.window).terminals.length === 0) {
+// 		vscode.window.showErrorMessage('No active terminals');
+// 		return false;
+// 	}
+// 	return true;
+// }
